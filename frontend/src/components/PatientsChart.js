753dbdec34a33f98945b28e35baff9ca56d7e692
Jerrod <jerrodwilliambush@gmail.com>
2023-04-04 13:59:35 -0500

    more styling

--- frontend/src/components/PatientsChart.js
+++ frontend/src/components/PatientsChart.js
@@ -43,7 +43,7 @@ function PatientsChart()
   ];
 
   return (
-    <ResponsiveContainer width="100%" height="85%">
+    <ResponsiveContainer width="100%" height="95%">
       <LineChart
         data={data}
         margin={{
