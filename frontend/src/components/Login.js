a8c54e2dce1949a63ce47d43fb3b5a134cb45a6c
Jerrod <jerrodwilliambush@gmail.com>
2023-04-04 15:33:47 -0500

    clean up

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -57,12 +57,10 @@ function Login({ setLoggedIn, setUserId })
             >
               <input name="email" type="text"
                 required onChange={handleChange}
-                // placeholder="email"
                 value="123@test.com"
               />
               <input name="password" type="password"
                 required onChange={handleChange}
-                // placeholder="password"
                 value="password"
               />
               <button type="submit">Login</button>
