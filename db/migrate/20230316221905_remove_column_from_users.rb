f511a0f2ba01ca21daa367800672ce14f3b8a0ae
Jerrod <jerrodwilliambush@gmail.com>
2023-04-04 09:27:22 -0500

    migration fix

--- db/migrate/20230316221905_remove_column_from_users.rb
+++ db/migrate/20230316221905_remove_column_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveColumnFromUsers < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :document, :string
+    remove_column :users, :document, :string if column_exists?(:users, :document)
   end
 end
